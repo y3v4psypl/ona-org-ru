8ac71c1558bece5f42ef0500224b374edf6f66d3
y3v4psypl <evavetrova@yahoo.com>
2023-08-19 00:32:19 -0600

    Added Twitter Posts API

---
+++ pages/api/twitter-posting.ts
@@ -0,0 +1,74 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors';
+import Parser from 'rss-parser';
+
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ['POST', 'GET', 'HEAD'],
+})
+
+
+
+const parser = new Parser<CustomItem>({
+    customFields: {
+        item: ['description']
+    }
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+const runMiddleware = (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    fn: Function
+) => {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result: any) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+
+            return resolve(result)
+        })
+    })
+}
+
+const getPosts = async () => {
+
+    const feed = await parser.parseURL('https://ona.org.ru/rss');
+
+    const posts: Post[] = [];
+
+    feed.items.forEach(item => {
+        posts.push(<Post>{
+            title: item.title,
+            description: item.description,
+            link: item.enclosure?.url
+        })
+    })
+
+    console.log(posts);
+
+    return posts;
+}
+
+const handler = async (
+    req: NextApiRequest,
+    res: NextApiResponse
+) => {
+    // Run the middleware
+    await runMiddleware(req, res, cors)
+
+
+    res.json(await getPosts());
+}
+
+export default handler;
+
+type Post = {
+    'title': string,
+    'description': string,
+    'link': string
+}
+
+type CustomItem = {description: string};
