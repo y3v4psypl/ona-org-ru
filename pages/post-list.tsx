8940daccd30cd9725c5ccfce4fa9a91b79764521
y3v4psypl <evavetrova@yahoo.com>
2023-08-19 02:46:01 -0600

    Added Twitter Post List page

---
+++ pages/post-list.tsx
@@ -0,0 +1,21 @@
+
+
+const getPostList = async (): Promise<Post[]> => {
+    const postList = await fetch('./api/twitter-posting');
+
+    return postList.json();
+}
+
+export default async function PostList() {
+    let postList: Post[] = await getPostList();
+    let postListItems = postList.map(post => <li>`🚺 ${post.title} 🚺\\n\\n` + `${post.link}`</li>)
+    return(
+        <ul>{postListItems}</ul>
+    )
+}
+
+type Post = {
+    'title': string,
+    'description': string,
+    'link': string
+}
