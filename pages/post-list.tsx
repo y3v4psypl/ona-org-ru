915a76105c171e3fd9c06b9c2477e7812f376717
y3v4psypl <evavetrova@yahoo.com>
2023-08-19 06:23:59 -0600

    Added Twitter Post List page

--- pages/post-list.tsx
+++ pages/post-list.tsx
@@ -44,7 +44,7 @@ export default function PostList() {
         let postList = getPostList().then((posts): Post[] => posts);
 
         postList.then(res => {
-            setPostListItems(res.map((post, i) => <li key={i}>{checkAndModifyPostText(post.description, post.title, post.link)}</li>))
+            setPostListItems(res.map((post, i) => <li key={i}>{checkAndModifyPostText(post.description, post.title, post.link).join(`${<br/>}`)}</li>))
             setState('success')
         })
 
