2bfd752405e470c50698f562e0ffba6ce3447941
y3v4psypl <evavetrova@yahoo.com>
2023-09-09 19:51:06 -0600

    Added "Cache-Control"-header needed for production to avoid Vercel's default response caching mechanism

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [{
+      source: '/api/twitter-posting',
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-cache, no-store, max-age=0, must-revalidate"
+        }
+      ]
+    }]
+  }
 }
 
 module.exports = nextConfig
